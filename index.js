846dad4220dc45eb1918c2f1d3440b80a4a22ef2
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-05-31 20:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ return test;
 }
 
 function increaseRankBy(n){
-  var lis=document.getElementById("app").querySelectorAll("ul.ranked");
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
 }
